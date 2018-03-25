86c0d2a9a57c423b25114caea3775a827cc1089c
Uttejkumar <uttej60@gmail.com>
2018-03-25 12:02:43 +0530

    added activities and set up bottomnav

--- app/src/main/java/com/uttej/oraclereadersclub/HomeActivity.java
+++ app/src/main/java/com/uttej/oraclereadersclub/HomeActivity.java
@@ -1,13 +1,22 @@
 package com.uttej.oraclereadersclub;
 
+import android.support.design.internal.BottomNavigationItemView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class HomeActivity extends AppCompatActivity {
+    private static final String TAG = "Home/BooksCatalogActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    /*
+    adding the bottom navigation view
+     */
+    private void setupBottomNavigationView(){
+        BottomNavigationItemView bottomNavigationItemView = (BottomNavigationItemView)findViewById(R.id.bottomNavigationBar);
     }
 }
---
+++ app/src/main/java/com/uttej/oraclereadersclub/ProfileActivity.java
@@ -0,0 +1,19 @@
+package com.uttej.oraclereadersclub;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Clean on 25-03-2018.
+ */
+
+public class ProfileActivity extends AppCompatActivity {
+    private static final String TAG = "ProfileActivity";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
