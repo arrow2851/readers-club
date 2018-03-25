9e3bbbefa90b0d0cc5bcddd482625638c63d40b6
Uttejkumar <uttej60@gmail.com>
2018-03-25 19:32:24 +0530

    images display in home using UIL

--- app/src/main/java/com/uttej/oraclereadersclub/Home/HomeActivity.java
+++ app/src/main/java/com/uttej/oraclereadersclub/Home/HomeActivity.java
@@ -6,14 +6,19 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.GridView;
 
 import com.uttej.oraclereadersclub.AddBooks.AddBooksActivity;
 import com.uttej.oraclereadersclub.Profile.ProfileActivity;
 import com.uttej.oraclereadersclub.R;
 import com.uttej.oraclereadersclub.Requests.RequestsActivity;
+import com.uttej.oraclereadersclub.Utils.GridImageAdapter;
+
+import java.util.ArrayList;
 
 public class HomeActivity extends AppCompatActivity {
     private static final String TAG = "Home/BooksCatalogActivity";
+    private int NUM_GRID_COLUMNS = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +26,7 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         setupBottomNavigationView();
+        tempGridInflator();
     }
 
     /*
@@ -38,17 +44,14 @@ public class HomeActivity extends AppCompatActivity {
                             case R.id.addBooks:
                                 Intent toAddBooks = new Intent(HomeActivity.this, AddBooksActivity.class);
                                 startActivity(toAddBooks);
-                                finish();
                                 break;
                             case R.id.requests:
                                 Intent toRequests = new Intent(HomeActivity.this, RequestsActivity.class);
                                 startActivity(toRequests);
-                                finish();
                                 break;
                             case R.id.userProfile:
                                 Intent toProfile = new Intent(HomeActivity.this, ProfileActivity.class);
                                 startActivity(toProfile);
-                                finish();
                                 break;
                             default:
                                 break;
@@ -57,6 +60,30 @@ public class HomeActivity extends AppCompatActivity {
                     }
                 }
         );
+    }
+
+    private void tempGridInflator(){
+        ArrayList<String> imgURLS = new ArrayList<>();
+        imgURLS.add("https://upload.wikimedia.org/wikipedia/commons/b/b4/JPEG_example_JPG_RIP_100.jpg");
+        imgURLS.add("http://www.personal.psu.edu/oeo5025/jpg.jpg");
+        imgURLS.add("https://upload.wikimedia.org/wikipedia/commons/4/41/Sunflower_from_Silesia2.jpg");
+        imgURLS.add("https://upload.wikimedia.org/wikipedia/commons/1/1e/Stonehenge.jpg");
+
+        setupImageGridView(imgURLS);
+    }
+
+    /*
+        displaying images in a grid view
+     */
+    private void setupImageGridView(ArrayList<String> imgURLs){
+        GridView gridView = (GridView)findViewById(R.id.imagesGrid);
+
+        int gridWidth = getResources().getDisplayMetrics().widthPixels;
+        int imageWidth = gridWidth/2;
+        gridView.setColumnWidth(imageWidth);
+
+        GridImageAdapter adapter = new GridImageAdapter(HomeActivity.this, R.layout.layout_grid_image_view, "", imgURLs);
+        gridView.setAdapter(adapter);
     }
 
 
---
+++ app/src/main/java/com/uttej/oraclereadersclub/Utils/SquareImageView.java
@@ -0,0 +1,30 @@
+package com.uttej.oraclereadersclub.Utils;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatImageView;
+import android.util.AttributeSet;
+
+/**
+ * Created by Clean on 25-03-2018.
+ */
+
+public class SquareImageView extends AppCompatImageView {
+
+    public SquareImageView(Context context) {
+        super(context);
+    }
+
+    public SquareImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SquareImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, widthMeasureSpec);
+    }
+}
