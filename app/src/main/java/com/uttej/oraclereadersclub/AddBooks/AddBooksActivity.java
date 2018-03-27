0f6dec307936705a6e615df6ad546c52a5e0b798
Uttejkumar <uttej60@gmail.com>
2018-03-27 07:17:39 +0530

    Added permissions to access camera

--- app/src/main/java/com/uttej/oraclereadersclub/AddBooks/AddBooksActivity.java
+++ app/src/main/java/com/uttej/oraclereadersclub/AddBooks/AddBooksActivity.java
@@ -1,10 +1,12 @@
 package com.uttej.oraclereadersclub.AddBooks;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
@@ -12,20 +14,58 @@ import com.uttej.oraclereadersclub.Home.HomeActivity;
 import com.uttej.oraclereadersclub.Profile.ProfileActivity;
 import com.uttej.oraclereadersclub.R;
 import com.uttej.oraclereadersclub.Requests.RequestsActivity;
+import com.uttej.oraclereadersclub.Utils.Permissions;
 
 /**
  * Created by Clean on 25-03-2018.
  */
 
 public class AddBooksActivity extends AppCompatActivity {
+
     private static final String TAG = "AddBooksActivity";
+
+    private static final int VERIFY_PERMISSION_REQUEST = 1;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_books);
 
-        setupBottomNavigationView();
+        if(checkPermissionsArray(Permissions.PERMISSIONS)){
+            
+         }
+        else{
+            verifyPermissions(Permissions.PERMISSIONS);
+        }
+//        setupBottomNavigationView();
+    }
+
+    public void verifyPermissions(String[] permissions){
+        ActivityCompat.requestPermissions(
+                AddBooksActivity.this,
+                permissions,
+                VERIFY_PERMISSION_REQUEST
+        );
+    }
+
+    public boolean checkPermissionsArray(String[] Permissions){
+        for(int i = 0; i < Permissions.length; i++){
+            String check = Permissions[i];
+            if(!checkPermissions(check)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean checkPermissions(String permission){
+        int permissionReuquest = ActivityCompat.checkSelfPermission(AddBooksActivity.this, permission);
+        if(permissionReuquest != PackageManager.PERMISSION_GRANTED){
+            return false;
+        }
+        else {
+            return true;
+        }
     }
 
     private void setupBottomNavigationView() {
---
+++ app/src/main/java/com/uttej/oraclereadersclub/Utils/Permissions.java
@@ -0,0 +1,17 @@
+package com.uttej.oraclereadersclub.Utils;
+
+import com.uttej.oraclereadersclub.Manifest;
+
+/**
+ * Created by Clean on 27-03-2018.
+ */
+
+public class Permissions {
+
+    public static final String[] PERMISSIONS = {
+            android.Manifest.permission.READ_EXTERNAL_STORAGE,
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            android.Manifest.permission.CAMERA
+    };
+
+}
